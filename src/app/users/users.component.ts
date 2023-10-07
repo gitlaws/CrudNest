80df762c69a0ac7d9e4ffb0d1a82aebec01829cd
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2023-10-07 13:50:31 -0400

    imported FormBuilder

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  userForm: any;
+  users: any;
 
+  constructor(public fb:FormBuilder) {
+    this.userForm = this.fb.group ({
+      Name: [""],
+      Email: [""],
+      Mobile: [""],
+      Age: [""],
+      id:[""],
+    })
+  }
+
+  ngOnInit(): void {}
 }
