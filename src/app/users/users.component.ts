1e45994b8338064fe1d19599dee086f6e3d5b5c4
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2023-10-10 20:30:06 -0400

    changes

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { CommonService } from 'src/app/common.service';
-declare var $:any
+declare var $: any;
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -11,19 +11,19 @@ export class UsersComponent implements OnInit {
   userForm: any;
   users: any;
 
-  constructor(public fb: FormBuilder) {
+  constructor(public fb: FormBuilder, private service: CommonService) {
     this.userForm = this.fb.group({
-      Name: [""],
-      Email: [""],
-      Mobile: [""],
-      Age: [""],
+      Name: [''],
+      Email: [''],
+      Mobile: [''],
+      Age: [''],
       id: [],
-    })
+    });
   }
 
   ngOnInit(): void {}
 
   SubmitForm() {
-    console.log(this.userForm.value)
+    console.log(this.userForm.value);
   }
 }
